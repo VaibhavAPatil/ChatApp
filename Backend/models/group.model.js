b30ca3d84dc593b2caf28f904caded8ac85fc416
VaibhavAPatil <vaibhavpatil.vap@gmail.com>
2025-03-15 13:37:13 +0530

    Added group.model.js

---
+++ Backend/models/group.model.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const grouSchema = new mongoose.Schema(
+  {
+    groupName: { type: String, required: true },
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    admin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Group", groupSchema);
